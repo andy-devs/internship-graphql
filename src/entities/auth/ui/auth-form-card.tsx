33c04f604d92bfe553b196e7772b60e7d3154c5b
Andrey Markushin <markushin.andrew@gmail.com>
2023-04-09 20:31:40 +0600

    fix: mobile height

--- src/entities/auth/ui/auth-form-card.tsx
+++ src/entities/auth/ui/auth-form-card.tsx
@@ -1,5 +1,6 @@
 import { SvgLogo } from '@shared/icons/components/logo';
 import { FC } from 'react';
+import { use100vh } from 'react-div-100vh';
 
 import { AuthTabs } from './auth-tabs';
 
@@ -8,9 +9,13 @@ interface AuthFormCardProps {
 }
 
 export const AuthFormCard: FC<AuthFormCardProps> = ({ children }) => {
+  const height = use100vh();
+
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center bg-grayscale100 px-2 py-5 dark:bg-grayscale700 xs:h-full xs:min-h-0 xs:max-w-[416px] xs:rounded-2xl xs:px-4.5">
-      <SvgLogo className="mb-4 min-h-[37px] " />
+    <div
+      className={`h-[${height}px] flex min-h-screen w-screen flex-col items-center bg-grayscale100 px-2 py-5 dark:bg-grayscale700 xs:h-full xs:min-h-0 xs:max-w-[416px] xs:rounded-2xl xs:px-4.5`}
+    >
+      <SvgLogo className="mb-4 min-h-[37px]" />
       <AuthTabs />
       <>{children}</>
     </div>
