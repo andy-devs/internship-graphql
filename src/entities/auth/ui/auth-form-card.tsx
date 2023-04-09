2ec78b8b56bae141421ca95c8eb04c7d93fda06b
Andrey Markushin <markushin.andrew@gmail.com>
2023-04-09 20:40:13 +0600

    fix: padding auth form

--- src/entities/auth/ui/auth-form-card.tsx
+++ src/entities/auth/ui/auth-form-card.tsx
@@ -1,6 +1,5 @@
 import { SvgLogo } from '@shared/icons/components/logo';
 import { FC } from 'react';
-import { use100vh } from 'react-div-100vh';
 
 import { AuthTabs } from './auth-tabs';
 
@@ -9,11 +8,9 @@ interface AuthFormCardProps {
 }
 
 export const AuthFormCard: FC<AuthFormCardProps> = ({ children }) => {
-  const height = use100vh();
-
   return (
     <div
-      className={`h-[${height}px] flex min-h-screen w-screen flex-col items-center bg-grayscale100 px-2 py-5 dark:bg-grayscale700 xs:h-full xs:min-h-0 xs:max-w-[416px] xs:rounded-2xl xs:px-4.5`}
+      className={`flex min-h-screen w-screen flex-col items-center bg-grayscale100 px-2 pb-[60px] pt-5 dark:bg-grayscale700 xs:h-full xs:min-h-0 xs:max-w-[416px] xs:rounded-2xl xs:py-5 xs:px-4.5`}
     >
       <SvgLogo className="mb-4 min-h-[37px]" />
       <AuthTabs />
